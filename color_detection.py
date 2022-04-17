e8f258393051386e02cb6049c63e9fcd2a2611c8
s-arslaan <shaikharslaan1234@gmail.com>
2022-04-17 20:38:53 +0530

    added originals

--- color_detection.py
+++ color_detection.py
@@ -1,8 +1,9 @@
+import csv
 import pandas as pd
 import cv2
 
 # imageUrl = 'image01.jpg'
-imageUrl = 'invitation.jpeg'
+imageUrl = 'color3.jpg'
 clicked = False
 redValue = 0
 greenValue = 0
@@ -10,20 +11,22 @@ blueValue = 0
 xPosition = 0
 yPosition = 0
 
-colorNameDataFrame = pd.read_csv('wikipedia_color_names.csv')
+colorNameDataFrame = pd.read_csv('colors.csv')
 colorNameDataFrame.drop(colorNameDataFrame.iloc[:,5:8], inplace=True, axis=1)
 colorNameDataFrame.rename(columns={'Hex (24 bit)':'Hex', 'Red (8 bit)':'Red', 'Green (8 bit)':'Green', 'Blue (8 bit)':'Blue'}, inplace=True)
 image = cv2.imread(imageUrl)
 
 def getColorName(red,green,blue):
     minimumValue = 10000
+    originalHex = '#%02x%02x%02x' % (red, green, blue)
     for i in range(len(colorNameDataFrame)):
         rgbValue = abs(red- int(colorNameDataFrame.loc[i,"Red"])) + abs(green- int(colorNameDataFrame.loc[i,"Green"]))+ abs(blue- int(colorNameDataFrame.loc[i,"Blue"]))
         if(rgbValue <= minimumValue):
             minimumValue = rgbValue
             colorName = colorNameDataFrame.loc[i,"Name"]
-            colorCode = colorNameDataFrame.loc[i,"Hex"]
-    return colorName+' : '+colorCode
+            colorHexCode = colorNameDataFrame.loc[i,"Hex"]
+            csv_rgb = str(colorNameDataFrame.loc[i,"Red"])+','+str(colorNameDataFrame.loc[i,"Green"])+','+str(colorNameDataFrame.loc[i,"Blue"])
+    return 'Input (RGB):'+str(red)+','+str(green)+','+str(blue)+' '+originalHex+' | '+colorName+' : '+colorHexCode+' : '+' ('+csv_rgb+')'
 
 def draw_function(event, x,y, flags, param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
@@ -43,7 +46,7 @@ if __name__ == '__main__':
     while (1):        
         if (clicked):            
             cv2.rectangle(image, (20, 20), (950, 60), (blueValue, greenValue, redValue), -1)
-            colorName = 'Selected Color :- ' + getColorName(redValue, greenValue, blueValue)
+            colorName = getColorName(redValue, greenValue, blueValue)
             cv2.putText(image, colorName, (50, 50), 2, 0.75, (255, 255, 255), 1, cv2.FONT_ITALIC)
             minimumValue = abs(redValue + greenValue + blueValue)
             if (minimumValue >= 600):
